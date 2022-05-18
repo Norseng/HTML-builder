ed4faa6505a2d98c1f48452d11a1e0bcd926b57a
Norseng <esina.helena@gmail.com>
2022-05-18 12:40:31 +0300

    feat: 04-copy-directory

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -10,12 +10,12 @@ readdir(filePath, {withFileTypes: true}, (err, files) => {
         files.forEach(file => {
             if (file.isFile()) {
                 let fileFullPath = path.join(filePath, file.name);
-                let fileProps = path.parse(fileFullPath);
+                let fileName = path.parse(fileFullPath);
                 lstat(fileFullPath, (err, stats) => {
                     if (err) {
                         console.log(err);
                     } else {
-                        console.log(fileProps.name + ' - ' + path.extname(fileFullPath).slice(1) + ' - ' + stats.size + 'b');
+                        console.log(fileName.name + ' - ' + path.extname(fileFullPath).slice(1) + ' - ' + stats.size + 'b');
                     }
                 });
             }
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const path = require('path');
+const {copyFile, mkdir, readdir} = require('fs');
+const filePath = path.join(__dirname, 'files');
+const fileCopyPath = path.join(__dirname, 'files-copy');
 
+const copyDir = () => {
+    mkdir(fileCopyPath, {recursive: true}, (err) => {
+        if (err) throw err;
+    });
+    readdir(filePath, {withFileTypes: true}, (err, files) => {
+        if (err)
+            console.log(err);
+        else {
+            files.forEach(file => {
+                if (file.isFile()) {
+                    let fileFullPath = path.join(filePath, file.name);
+                    let fileName = path.parse(fileFullPath).base;                
+                    let oldfilePath = path.join(filePath, fileName);
+                    let NewfilePath = path.join(fileCopyPath, fileName);
+                    copyFile(oldfilePath, NewfilePath, (err) => {
+                        if (err) throw err;
+                        console.log(fileName + ' was copied to files-copy');
+                    });
+                }
+            })
+        }
+    })
+}
+
+copyDir();
