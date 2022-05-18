d4ae0672a5caea2787e5257476f70a31e616eeed
Norseng <esina.helena@gmail.com>
2022-05-18 13:15:07 +0300

    feat: 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,30 @@
+const {fs, readdir, readFile, appendFile} = require('fs');
+const path = require('path');
+const distPath = path.join(__dirname, 'project-dist');
+const stylesPath = path.join(__dirname, 'styles');
+const filePath = path.join(distPath, 'bundle.css');
+
+
+
+readdir(stylesPath, {withFileTypes: true}, (err, files) => {
+    if (err) console.log(err);
+    else {
+        files.forEach(file => {
+            if (file.isFile()) {
+                let fileFullPath = path.join(stylesPath, file.name);
+                let fileName = path.parse(fileFullPath).ext.slice(1);
+                let fileStylePath = path.join(stylesPath, path.parse(fileFullPath).base);
+                if(fileName === 'css') {
+                    readFile(fileStylePath, 'utf8', (err, data) => {
+                        if (err) console.log(err);
+                        appendFile(filePath, data, 'utf8', (err) => {
+                            if (err) throw err;
+                            console.log('\n' + path.parse(fileFullPath).base + ' was appended to bundle.css!');
+                        });
+                    });
+                    
+                }
+            }
+        })
+    }
+})
